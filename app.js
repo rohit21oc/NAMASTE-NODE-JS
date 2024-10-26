046ac513f4fa8ff00a316ba2780c5f0bd1fbaab2
rohit21oc <rohitkumar15031@gmail.com>
2024-10-27 01:50:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const {x,calculateSum} = require("./math");
+var name= "Rohit Kumar";
+
+calculateSum(20,50);
+console.log(x);
+
+
+let a = 10;
+let b = 20;
+console.log(a+b);
+console.log(name);
+
+console.log(globalThis === global);
+
+
---
+++ math.js
@@ -0,0 +1,8 @@
+function calculateSum(a,b){
+    let sum = a+b;
+    console.log(sum);
+}
+
+const x = "Heyyy!"
+
+module.exports = {x,calculateSum};
